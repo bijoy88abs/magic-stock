20c6055db2a9c7c02e61d878f0e3f0ec4cf45e13
bijoy88abs <bijoyduttajis88@gmail.com>
2021-07-28 11:48:03 +0530

    commit

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const app = express();
 
+
+app.get('/', (req, res) => {
+    res.send('listening');
+})
+
+
 const PORT = process.env.PORT || 8050;
 
 app.listen(PORT, () => {
