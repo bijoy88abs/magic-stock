fec4654e89fb9f36c449f67a370aefcfc068ee58
bijoy88abs <bijoyduttajis88@gmail.com>
2021-07-28 21:18:30 +0530

    c

--- app.js
+++ app.js
@@ -5,7 +5,13 @@ const mongoose = require("mongoose");
 
 app.use(cors())
 const stockMasterRouter = require("./masterRouter");
+app.use(
+    express.urlencoded({
+        extended: true
+    })
+);
 
+app.use(express.json());
 
 app.use('/master-stock', stockMasterRouter);
 
@@ -13,7 +19,7 @@ app.use('/master-stock', stockMasterRouter);
 const PORT = process.env.PORT || 8050;
 mongoose
     .connect(`mongodb+srv://magicmoment:8420541261Atlas@cluster0.cxiug.mongodb.net/stock?retryWrites=true&w=majority`,
-        { useNewUrlParser: true , useUnifiedTopology: true})
+        { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         app.listen(PORT, () => {
             console.log('server running');
