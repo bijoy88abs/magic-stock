1ff1d38f3584c7908b6282d5fa98fa8c7ab68275
bijoy88abs <bijoyduttajis88@gmail.com>
2021-07-29 17:29:23 +0530

    c

--- masterRouter.js
+++ masterRouter.js
@@ -1,9 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const router = express.Router();
-
 let schema = mongoose.Schema;
-
 
 let masterPurchaseItemSchema = new schema({
     itemName: String
@@ -11,28 +9,57 @@ let masterPurchaseItemSchema = new schema({
 let masterPurchaseItemModel = mongoose.model('masterPurchaseItem', masterPurchaseItemSchema);
 
 router.post('/masterPurchaseItemPost', (req, res) => {
-    console.log('req-------', req.body);
     let masterPurchaseItemModelQuery = new masterPurchaseItemModel({
         itemName: req.body.itemName
     });
 
     masterPurchaseItemModelQuery.save().then(() => {
-        res.json({msg: 'masterPurchaseItemModelQuery executed'});
+        res.json({ msg: 'masterPurchaseItemModelQuery executed' });
     }).catch((e) => {
         res.json(e);
     })
 });
 
+router.post('/masterPurchaseItemEdit', (req, res) => {
+    console.log('req-------', req.body);
+    let { itemName } = req.body;
+    let { id } = req.body;
+
+    masterPurchaseItemModel.findOneAndUpdate(
+        {
+            _id: id
+        },
+        {
+            $set: { itemName }
+        }
+    ).then(() => {
+        res.json({ msg: 'masterPurchaseItemModelQuery updated' });
+    }).catch((e) => {
+        res.json(e);
+    });
+});
+
 router.get('/masterPurchaseItemView', (req, res) => {
     masterPurchaseItemModel.find().then((responseData) => {
-        res.json({data: responseData});
+        res.json({ data: responseData });
     }).catch((e) => {
         res.json(e);
     });
 });
 
 router.post('/masterPurchaseItemDelete', (req, res) => {
-   
+    console.log('req-------', req.body);
+    let { id } = req.body;
+
+    masterPurchaseItemModel.findOneAndDelete(
+        {
+            _id: id
+        }
+    ).then(() => {
+        res.json({ msg: 'masterPurchaseItemModelQuery deleted' });
+    }).catch((e) => {
+        res.json(e);
+    });
 });
 
 module.exports = router;
