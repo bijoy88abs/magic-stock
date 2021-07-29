981fb3a5c52b74afa8d5cd72d7500a6844f0b082
bijoy88abs <bijoyduttajis88@gmail.com>
2021-07-29 18:24:32 +0530

    c

--- masterRouter.js
+++ masterRouter.js
@@ -62,4 +62,118 @@ router.post('/masterPurchaseItemDelete', (req, res) => {
     });
 });
 
+
+/************************************************************************************ */
+/************************************************************************************ */
+let masterPurchaseRawItemSchema = new schema({
+    itemName: String
+});
+let masterPurchaseRawItemModel = mongoose.model('masterPurchaseRawItem', masterPurchaseRawItemSchema);
+
+router.post('/masterPurchaseRawItemPost', (req, res) => {
+    let masterPurchaseRawItemModelQuery = new masterPurchaseRawItemModel({
+        itemName: req.body.itemName
+    });
+    masterPurchaseRawItemModelQuery.save().then(() => {
+        res.json({ msg: 'masterPurchaseRawItemModelQuery executed' });
+    }).catch((e) => {
+        res.json(e);
+    })
+});
+
+router.post('/masterPurchaseRawItemEdit', (req, res) => {
+    let { itemName } = req.body;
+    let { id } = req.body;
+    masterPurchaseRawItemModel.findOneAndUpdate(
+        {
+            _id: id
+        },
+        {
+            $set: { itemName }
+        }
+    ).then(() => {
+        res.json({ msg: 'masterPurchaseRawItemModelQuery updated' });
+    }).catch((e) => {
+        res.json(e);
+    });
+});
+
+router.get('/masterPurchaseRawItemView', (req, res) => {
+    masterPurchaseRawItemModel.find().then((responseData) => {
+        res.json({ data: responseData });
+    }).catch((e) => {
+        res.json(e);
+    });
+});
+
+router.post('/masterPurchaseRawItemDelete', (req, res) => {
+    let { id } = req.body;
+    masterPurchaseRawItemModel.findOneAndDelete(
+        {
+            _id: id
+        }
+    ).then(() => {
+        res.json({ msg: 'masterPurchaseRawItemModelQuery deleted' });
+    }).catch((e) => {
+        res.json(e);
+    });
+});
+
+/************************************************************************************ */
+/************************************************************************************ */
+let masterPurchaseMachineItemSchema = new schema({
+    itemName: String
+});
+let masterPurchaseMachineItemModel = mongoose.model('masterPurchaseMachineItem', masterPurchaseMachineItemSchema);
+
+router.post('/masterPurchaseMachineItemPost', (req, res) => {
+    let masterPurchaseMachineItemModelQuery = new masterPurchaseMachineItemModel({
+        itemName: req.body.itemName
+    });
+    masterPurchaseMachineItemModelQuery.save().then(() => {
+        res.json({ msg: 'masterPurchaseMachineItemModelQuery executed' });
+    }).catch((e) => {
+        res.json(e);
+    })
+});
+
+router.post('/masterPurchaseMachineItemEdit', (req, res) => {
+    let { itemName } = req.body;
+    let { id } = req.body;
+    masterPurchaseMachineItemModel.findOneAndUpdate(
+        {
+            _id: id
+        },
+        {
+            $set: { itemName }
+        }
+    ).then(() => {
+        res.json({ msg: 'masterPurchaseMachineItemModelQuery updated' });
+    }).catch((e) => {
+        res.json(e);
+    });
+});
+
+router.get('/masterPurchaseMachineItemView', (req, res) => {
+    masterPurchaseMachineItemModel.find().then((responseData) => {
+        res.json({ data: responseData });
+    }).catch((e) => {
+        res.json(e);
+    });
+});
+
+router.post('/masterPurchaseMachineItemDelete', (req, res) => {
+    let { id } = req.body;
+    masterPurchaseMachineItemModel.findOneAndDelete(
+        {
+            _id: id
+        }
+    ).then(() => {
+        res.json({ msg: 'masterPurchaseMachineItemModelQuery deleted' });
+    }).catch((e) => {
+        res.json(e);
+    });
+});
+
+
 module.exports = router;
